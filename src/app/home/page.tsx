f3cdfd3d1df8140ffbdd65b89624285ca093fb9f
Christian Anthony Dumanauw <chrisdumanauw@gmail.com>
2025-07-26 22:20:38 +0800

    add navbar and footer

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Image from "next/image";
 import sunsetHikeBg from "../../../public/assets/sunset_hike_bg.jpg";
+import Navbar from '@/components/navbar';
+import Footer from '@/components/footer';
 
 const HomePage = () => {
     return (
         <>
+            <Navbar />
             <div className="h-full">
                 <div className="">
                     <Image
@@ -16,8 +19,8 @@ const HomePage = () => {
                 <div className="flex justify-center p-15">
                     <h1 className="font-bold text-8xl">WELCOME BLA BLA BLA</h1>
                 </div>
-                
             </div>
+            <Footer />
         </>
     )
 }
