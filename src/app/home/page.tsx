33eb7e041e9a4779bc70fc8c00a52e1cf69e11ee
Christian Anthony Dumanauw <chrisdumanauw@gmail.com>
2025-07-26 11:50:00 +0800

    set full screen

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const HomePage = () => {
     return (
-        <></>
+        <>
+            <div className="h-screen">
+
+            
+            </div>
+        </>
     )
 }
 
