403104ec9f006618dca38ceb8cbc7a956f6310b3
Christian Anthony Dumanauw <chrisdumanauw@gmail.com>
2025-07-26 18:28:21 +0800

    add simple background in main

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Image from "next/image";
+import sunsetHikeBg from "../../../public/assets/sunset_hike_bg.jpg";
 
 const HomePage = () => {
     return (
         <>
-            <div className="h-screen">
-            
+            <div className="h-full">
+                <div className="">
+                    <Image
+                        src={sunsetHikeBg}
+                        className="w-full h-fit"
+                        alt="bg-main-1"
+                    ></Image>
+                </div>
+                <div className="flex justify-center p-15">
+                    <h1 className="font-bold text-8xl">WELCOME BLA BLA BLA</h1>
+                </div>
+                
             </div>
         </>
     )
