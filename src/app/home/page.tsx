aba28ff1bfcfc48809f546a73cb080676290f551
Sean Darren Timothy <seandarren00@gmail.com>
2025-07-27 01:30:45 +0800

    gradient background and styled headings

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,28 +1,40 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import sunsetHikeBg from "../../../public/assets/sunset_hike_bg.jpg";
-import Navbar from '@/components/navbar';
-import Footer from '@/components/footer';
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+import { Roboto_Slab, Inter } from "next/font/google";
+
+const robotoSlab = Roboto_Slab({
+  subsets: ["latin"],
+  weight: "700",
+});
+
+const fontInter = Inter({
+  subsets: ["latin"],
+});
 
 const HomePage = () => {
-    return (
-        <>
-            <Navbar />
-            <div className="h-full">
-                <div className="">
-                    <Image
-                        src={sunsetHikeBg}
-                        className="w-full h-fit"
-                        alt="bg-main-1"
-                    ></Image>
-                </div>
-                <div className="flex justify-center p-15">
-                    <h1 className="font-bold text-8xl">WELCOME TO CITY UNIVERSITY OF HONG KONG</h1>
-                </div>
-            </div>
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      <div className="min-h-screen">
+        <section className="bg-gradient-to-b from-red-50 to-white py-30">
+          <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 text-center">
+            <h1 className={`text-4xl md:text-6xl font-bold mb-3 text-gray-800 ${fontInter.className}`}>
+              Welcome to
+            </h1>
+            <h2
+              className={`text-6xl md:text-8xl font-bold text-red-700 mb-6 ${robotoSlab.className}`}
+            >
+              PERMISI HK
+            </h2>
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default HomePage
+export default HomePage;
