94cf99d600e27b8daadbb30bd50679ce34d98d6b
christiandumanauw <christian.dumanauw@generali.com.hk>
2025-09-02 15:46:10 +0800

    temp1

--- src/components/properties/adminPanelItemProp.ts
+++
@@ -1,9 +0,0 @@
-import { Ref } from "react";
-
-export default interface AdminPanelItemProp{
-    routePath: string;
-    onClick: () => void;
-    text: string;
-    icon: string;
-    ref?: Ref<HTMLDivElement>;
-}
--- src/components/properties/adminPanelProp.ts
+++
@@ -1,8 +0,0 @@
-import AdminPanelItemProp from "./AdminPanelItemProp";
-
-export default interface AdminPanelProp{
-    numberOfItem: number;
-    listOfItem: AdminPanelItemProp[];
-    handleClick: () => void;
-    itemsRef: React.RefObject<(HTMLDivElement | null)[]>;
-}
