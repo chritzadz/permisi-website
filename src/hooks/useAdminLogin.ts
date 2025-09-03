9d13b46126a33e2690054d561fd214039ca35eb8
Sean Darren Timothy <seandarren00@gmail.com>
2025-09-03 22:30:51 +0800

    setup login hooks

---
+++ src/hooks/useAdminLogin.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+export const useAdminLogin = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    checkLoginStatus();
+  }, []);
+
+  const checkLoginStatus = async () => {
+    try {
+      const response = await fetch("api/admin/validate");
+      const data = await response.json();
+
+      setIsAuthenticated(data.success);
+    } catch {
+      setIsAuthenticated(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const login = async (username: string, password: string) => {
+    try {
+      const response = await fetch("/admin/auth", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      if (data.success) {
+        setIsAuthenticated(true);
+        return { success: true };
+      }
+
+      return { success: false, message: "Network Error" };
+    } catch {
+      return { success: false, message: "Network Error" };
+    }
+  };
+  const logout = async () => {
+    document.cookie =
+      "admin-token=; Path=/; Expires=Fri, 03 Sep 2025 00:00:01 GMT;";
+    setIsAuthenticated(false);
+  };
+
+  return { isAuthenticated, isLoading, login, logout, checkLoginStatus };
+};
