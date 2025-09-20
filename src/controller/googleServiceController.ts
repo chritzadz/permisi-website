a4bd349a63473e1ba08614a68083459ef0575786
chritzadz <chrisdumanauw@gmail.com>
2025-09-20 16:15:26 +0800

    fix attempt sanitized key

--- src/controller/googleServiceController.ts
+++ src/controller/googleServiceController.ts
@@ -6,11 +6,15 @@ export class GoogleServiceController {
     private auth;
 
     constructor(spreadSheetId: string) {
+        const rawGooglePrivateKey = process.env.GOOGLE_PRIVATE_KEY || "";
+        const sanitizedKey = rawGooglePrivateKey.replace(/\\n/g, "\n");
+
+
         const credentials = {
             type: 'service_account',
             project_id: 'permisi-website-main',
             private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-            private_key: process.env.GOOGLE_PRIVATE_KEY,
+            private_key: sanitizedKey,
             client_email: process.env.GOOGLE_CLIENT_EMAIL,
             client_id: process.env.GOOGLE_CLIENT_ID,
             auth_uri: "https://accounts.google.com/o/oauth2/auth",
